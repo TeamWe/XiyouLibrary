1001b8229b5b1df062b98ff17417dd3a1f728fee
TeamWe <mrxujg@gmail.com>
2016-07-29 23:19:14 +0800

    new put

---
+++ xiyouLibaryTests/_____Tests.m
@@ -0,0 +1,39 @@
+//
+//  _____Tests.m
+//  西邮图书馆Tests
+//
+//  Created by 王建刚 on 15/10/17.
+//  Copyright © 2015年 王建刚. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _____Tests : XCTestCase
+
+@end
+
+@implementation _____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
